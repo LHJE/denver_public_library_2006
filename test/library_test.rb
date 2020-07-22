17e1d49006f39edbe8278015e4ed6b0621cf7cbf
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-22 13:37:37 -0600

    Lib has books test updated

--- test/library_test.rb
+++ test/library_test.rb
@@ -32,12 +32,16 @@ class LibraryTest < Minitest::Test
   end
 
   def test_library_has_books
+    dpl = Library.new("Denver Public Library")
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
     charlotte_bronte.write("Jane Eyre", "October 16, 1847")
     professor = charlotte_bronte.write("The Professor", "1857")
     villette = charlotte_bronte.write("Villette", "1853")
     harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
     mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
 
     assert_equal [professor, villette, harper_lee, mockingbird], dpl.books
   end
