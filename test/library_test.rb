c2a6c4c6c28b6245a32051498803c39700209a62
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-22 14:21:31 -0600

    checkout test added

--- test/library_test.rb
+++ test/library_test.rb
@@ -31,21 +31,24 @@ class LibraryTest < Minitest::Test
     assert_equal [charlotte_bronte, harper_lee], dpl.authors
   end
 
-  # def test_library_has_books
-  #   dpl = Library.new("Denver Public Library")
-  #   charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
-  #   jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
-  #   professor = charlotte_bronte.write("The Professor", "1857")
-  #   villette = charlotte_bronte.write("Villette", "1853")
-  #   harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
-  #   mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
-  #
-  #   dpl.add_author(charlotte_bronte)
-  #   dpl.add_author(harper_lee)
-  #
-  #
-  #   assert_equal [jane_eyre, professor, villette, mockingbird], dpl.books
-  # end
+  def test_library_has_books
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    # binding.pry
+    professor = charlotte_bronte.write("The Professor", "1857")
+    # binding.pry
+    villette = charlotte_bronte.write("Villette", "1853")
+    # binding.pry
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    binding.pry
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+
+    assert_equal [jane_eyre, professor, villette, mockingbird], dpl.books
+  end
 
   def test_publication_time_frame_for
     # skip
@@ -92,6 +95,21 @@ class LibraryTest < Minitest::Test
 
     assert_equal result, dpl.publication_time_frame_for(charlotte_bronte)
     assert_equal result2, dpl.publication_time_frame_for(harper_lee)
+  end
+
+  def test_checkout_false
+    # skip
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+
+    assert_equal false, dpl.checkout(jane_eyre)
+    assert_equal false, dpl.checkout(mockingbird)
   end
 
 end
