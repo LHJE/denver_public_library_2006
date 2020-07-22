b5ea06b4c9f19d3ae269069d7712cb6616ae8f08
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-22 13:28:53 -0600

    Lib attributes test passed

--- lib/library.rb
+++ lib/library.rb
@@ -2,10 +2,12 @@ require 'pry'
 
 class Library
 
-  attr_reader :name
+  attr_reader :name, :books, :authors
 
   def initialize(name)
     @name = name
+    @books = []
+    @authors = []
   end
 
 end
