c39d8eed8895178fbe25912a684d34cd54ee8b6a
Luke Hunter James-Erickson <theeluke@gmail.com>
2020-07-22 13:36:24 -0600

    library.books test added

--- test/library_test.rb
+++ test/library_test.rb
@@ -31,6 +31,15 @@ class LibraryTest < Minitest::Test
     assert_equal [charlotte_bronte, harper_lee], dpl.authors
   end
 
+  def test_library_has_books
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
 
+    assert_equal [professor, villette, harper_lee, mockingbird], dpl.books
+  end
 
 end
